d7c02ef53db77c3a022fb7d5a25b4ec736acdb91
Vasil Topalovic <vasiltopalovic@outlook.com>
2024-11-05 14:36:48 -0500

    feat: make top bar smaller

--- projects/dmenu/config.h
+++ projects/dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono NFM:size=10"
+	"JetBrainsMono NFM:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -22,5 +22,5 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 
-static unsigned int lineheight = 26;
+static unsigned int lineheight = 22;
 static unsigned int min_lineheight = 8;
